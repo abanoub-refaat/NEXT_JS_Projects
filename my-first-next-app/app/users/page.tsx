d4c751bf4872e02c380336adea32c2d94c6907eb
abanoub-refaat <abanoubref3at18@gmail.com>
2024-12-09 12:41:23 +0200

    Fetching data and TypeScript type annotaion

--- my-first-next-app/app/users/page.tsx
+++ my-first-next-app/app/users/page.tsx
@@ -1,7 +1,42 @@
 import React from "react";
 
-const UserPage = () => {
-  return <div>UserPage</div>;
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
+
+const UserPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await res.json();
+
+  return (
+    <>
+      <h1>Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default UserPage;
