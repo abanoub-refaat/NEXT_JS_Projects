8b7b373a1cda447cccf3f448e2073b930f1a33e2
abanoub-refaat <abanoubref3at18@gmail.com>
2024-12-09 13:15:09 +0200

    Caches, Client-side and Sever-side Rendering

--- my-first-next-app/app/users/page.tsx
+++ my-first-next-app/app/users/page.tsx
@@ -23,13 +23,24 @@ interface User {
   };
 }
 
+/*
+ * Caching is storing data somewhere that is faster to access.
+ * Revalidate is how often the data should be re-fetched
+ * We can set {chache: "no-store"} to disable caching or set {next: {revalidate: 10}} to revalidate every 10 seconds
+ * inside the fetch function.
+ */
 const UserPage = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const users: User[] = await res.json();
 
+  /*
+   * Rendering can happend on the client side and the server side
+   * The client side rendering is faster and more efficient
+   */
   return (
     <>
       <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
