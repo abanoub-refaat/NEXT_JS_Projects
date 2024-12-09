08063c8e6b4a6cce47bab6de4a7a2ab8ce2551f2
abanoub-refaat <abanoubref3at18@gmail.com>
2024-12-09 13:16:06 +0200

    Caches, Client-side and Sever-side Rendering

--- my-first-next-app/app/users/page.tsx
+++ my-first-next-app/app/users/page.tsx
@@ -30,7 +30,9 @@ interface User {
  * inside the fetch function.
  */
 const UserPage = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    cache: "no-store",
+  });
   const users: User[] = await res.json();
 
   /*
