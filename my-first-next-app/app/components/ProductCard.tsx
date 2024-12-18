b1df29207588b158d06536693cd0548eb78165bb
abanoub-refaat <abanoubref3at18@gmail.com>
2024-12-18 19:26:08 +0200

    tailwind and daisyui intro

--- my-first-next-app/app/components/ProductCard.tsx
+++ my-first-next-app/app/components/ProductCard.tsx
@@ -1,16 +1,14 @@
-// "use client";
-
 /*
+ *"use client";
  * use client is used to render the component on the client side
  * here, AddToCart component is rendered on the client side but ProductCard component is rendered on the server side
  */
 import React from "react";
 import AddToCart from "./AddToCart";
-import styles from "./ProductCard.module.css";
 
 const ProductCard = () => {
   return (
-    <div className={styles.card}>
+    <div>
       <AddToCart />
     </div>
   );
